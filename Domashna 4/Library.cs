5ea4fd90bcee82f7ba40a969830057725680e6d0
Radoinov <radoinov08@gmail.com>
2024-11-05 19:04:34 +0200

    Domashna 4

---
+++ Domashna 4/Library.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domashna_4
+{
+    internal class Library
+    {
+        ArrayList books = new ArrayList();
+
+
+        public void AddBook(string title, string author)
+        {
+            Book newBook = new Book(title, author);
+            books.Add(newBook);
+            Console.WriteLine("Knigata e uspeshno dobavena.");
+        }
+
+        
+       
+        
+            public void RemoveBook(string title)
+            {
+                Book bookToRemove = null;
+                foreach (Book book in books)
+                {
+                    if (book.Title.ToLower() == title.ToLower())
+                    {
+                        bookToRemove = book;
+                        break;
+                    }
+                }
+
+                if (bookToRemove != null)
+                {
+                    books.Remove(bookToRemove);
+                    Console.WriteLine("Knigata beshe uspeshno premahnata.");
+                }
+                else
+                {
+                    Console.WriteLine("Kniga s tova zaglavie ne beshe namerena.");
+                }
+            }
+
+
+
+        public void SearchBook(string title)
+        {
+            Book foundBook = null;
+            foreach (Book book in books)
+            {
+                if (book.Title.ToLower() == title.ToLower())
+                {
+                    foundBook = book;
+                    break;
+                }
+            }
+
+            if (foundBook != null)
+            {
+                Console.WriteLine("Namerena kniga:");
+                Console.WriteLine(foundBook);
+            }
+            else
+            {
+                Console.WriteLine("Kniga s tova zaglavie ne beshe namerena.");
+            }
+        }
+
+
+        public void ListAllBooks()
+        {
+            if (books.Count > 0)
+            {
+                Console.WriteLine("Knigi v bibliotekata:");
+                foreach (Book book in books)
+                {
+                    Console.WriteLine(book);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Bibliotekata e prazna.");
+            }
+        }
+    }
+
+}
+
---
+++ Domashna 4/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Domashna_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Library library = new Library();
+            while (true)
+            {
+                Console.WriteLine("Izberi opciq:");
+                Console.WriteLine("1 - Dobavqne na kniga");
+                Console.WriteLine("2 - Premahvane na kniga po zaglavie");
+                Console.WriteLine("3 - Tyrsene na kniga po zaglavie");
+                Console.WriteLine("4 - Izvejdane na vsichki knigi");
+                Console.WriteLine("5 - Izhod");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        Console.Write("Vavedi zaglavie na knigata: ");
+                        string title = Console.ReadLine();
+                        Console.Write("Vavedi avtor na knigata: ");
+                        string author = Console.ReadLine();
+                        library.AddBook(title, author);
+                        break;
+
+                    case "2":
+                        Console.Write("Vavedete zaglavie na knigata, koqto iskate da premahnete: ");
+                        title = Console.ReadLine();
+                        library.RemoveBook(title);
+                        break;
+
+                    case "3":
+                        Console.Write("Vavedete zaglavie za tyrsene: ");
+                        title = Console.ReadLine();
+                        library.SearchBook(title);
+                        break;
+
+                    case "4":
+                        library.ListAllBooks();
+                        break;
+
+                    case "5":
+                        Console.WriteLine("Izhod ot programata.");
+                        break;
+
+                       
+                }
+            }
+        }
+    }
+ }
+
